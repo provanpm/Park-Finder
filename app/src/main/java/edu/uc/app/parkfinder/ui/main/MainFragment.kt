61a3f4be00e53c6399fcd19c893b2bd09293be5e
Burneta3 <78327737+Burneta3@users.noreply.github.com>
2021-03-10 20:22:52 -0500

    My suggestions for code review

--- app/src/main/java/edu/uc/app/parkfinder/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/app/parkfinder/ui/main/MainFragment.kt
@@ -11,9 +11,7 @@ import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
+
 
     private lateinit var viewModel: MainViewModel
 
@@ -24,5 +22,8 @@ class MainFragment : Fragment(R.layout.main_fragment) {
                 parks -> actPark.setAdapter(ArrayAdapter(requireContext(), R.layout.support_simple_spinner_dropdown_item, parks))
         })
 
+    }
+    companion object {
+        fun newInstance() = MainFragment()
     }
 }
