d608599899617cd020f050c5784e315d79ca0207
Paul Provan <provanpm@mail.uc.edu>
2021-04-07 16:11:05 -0400

    Added UI in main_activity for bottom menu bar

--- app/src/main/java/edu/uc/app/parkfinder/dto/Park.kt
+++ app/src/main/java/edu/uc/app/parkfinder/dto/Park.kt
@@ -5,7 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class Park(@SerializedName("Location Name") var name: String = "",
                 @SerializedName("Location Number") var number: String = "",
                 var description: String = "",
-                var address: String = "") {
+                var address: String = "",
+                var parkID: String = "") {
 
     override fun toString() = "$number $name"
 }
--- app/src/main/java/edu/uc/app/parkfinder/ui/main/MainViewModel.kt
+++ app/src/main/java/edu/uc/app/parkfinder/ui/main/MainViewModel.kt
@@ -1,6 +1,7 @@
 package edu.uc.app.parkfinder.ui.main
 
 import android.util.Log
+import android.content.ContentValues.TAG
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
@@ -9,7 +10,13 @@ import edu.uc.app.parkfinder.dto.Park
 import edu.uc.app.parkfinder.service.ParkService
 
 class MainViewModel : ViewModel() {
+
+    // uses Retrofit & uses GitHub JSON
     var parks: MutableLiveData<ArrayList<Park>> = MutableLiveData<ArrayList<Park>>()
+
+    // uses Firebase & Firestore
+    var allParks: MutableLiveData<ArrayList<Park>> = MutableLiveData<ArrayList<Park>>()
+
     private var parkService: ParkService = ParkService()
     private lateinit var firestore : FirebaseFirestore
 
@@ -23,14 +30,36 @@ class MainViewModel : ViewModel() {
         parks = parkService.fetchParks()
     }
 
+    /*
+     * Hears any updates to Firestore
+     */
+    private fun listenToParks() {
+        firestore.collection("parks").addSnapshotListener {
+            snapshot, e ->
+            if (e != null) {
+                Log.w(TAG, "Listen Failed", e)
+                return@addSnapshotListener
+            }
+
+            if (snapshot != null) {
+                val allParksTemp = ArrayList<Park>()
+                val documents = snapshot.documents
+                documents.forEach {
+                    it.toObject(Park::class.java)
+                }
+                allParks.value = allParksTemp
+            }
+        }
+    }
+
     fun save(park: Park) {
-        firestore.collection("parks")
-            .document()
-            .set(park)
-            .addOnSuccessListener {
+        val document = firestore.collection("parks").document()
+        park.parkID = document.id
+        val set = document.set(park)
+            set.addOnSuccessListener {
                 Log.i("Firebase", "document saved")
             }
-            .addOnFailureListener {
+            set.addOnFailureListener {
                 Log.e("Firebase", "save failed")
             }
     }
