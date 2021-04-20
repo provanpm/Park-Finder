d60707617adef4f6aded9731cc3422601baee643
Paul Provan <provanpm@mail.uc.edu>
2021-04-20 14:35:56 -0400

    Fixed double home fragment on initial app load

--- app/src/main/java/edu/uc/app/parkfinder/ui/main/MainActivity.kt
+++ app/src/main/java/edu/uc/app/parkfinder/ui/main/MainActivity.kt
@@ -11,11 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.home_fragment, HomeFragment.newInstance())
-                    .commitNow()
-        }
 
         buttonMenuSearch.setOnClickListener {
             Navigator.decideNav("SearchFragment", this)
